8dfb3798817f8971557c49d7bf2428125fdf9d5a
LuckyPhoenix7 <a.lubashov@gmail.com>
2023-11-23 23:53:44 +0100

    HW#4-images v1.1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,31 @@
 import { useState } from 'react';
 import ReactModal from 'react-modal';
 
+const modalStyles = {
+  content: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    border: '1px solid #ccc',
+    background: '#fff',
+    overflow: 'auto',
+    borderRadius: '10px',
+    outline: 'none',
+    padding: '20px',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  },
+};
+
 ReactModal.setAppElement('#root');
 
 export const Modal = ({ largeImageURL, tags, isOpen, onCloseModal }) => {
-  const [modalStyles, setModalStyles] = useState({
-    content: {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      border: '1px solid #ccc',
-      background: '#fff',
-      overflow: 'auto',
-      borderRadius: '10px',
-      outline: 'none',
-      padding: '20px',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    },
-    overlay: {
-      backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    },
-  });
-
   return (
     <ReactModal
       isOpen={isOpen}
